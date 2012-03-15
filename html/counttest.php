0af7dab2ce2e0574b336d4f3af33203146f6e2ec
ngsw <ngsw@ngsw.jp>
2012-03-16 03:31:39 +0900

    deleted:    html/counttest.php

--- html/counttest.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include_once ('../lib/instagram.php');
-  include_once ('../lib/instagrammysqli.php');
-
-  $insta = new InstagramMySQLi;
-  $insta->insta_header();
-  $insta->insta_rand();
-  $insta->insta_footer();
